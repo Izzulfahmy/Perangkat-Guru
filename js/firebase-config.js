3d3ce99940315ba9c5baf41e8a4319170ecfd2e5
Izzulfahmy <izzuley123@gmail.com>
2026-07-19 05:44:03 +0700

    de

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,27 +1,25 @@
 // js/firebase-config.js
-(function () {
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-  import { getFirestore, collection, getDocs, setDoc, doc, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
-  import { getAuth, signInAnonymously, signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore, collection, getDocs, setDoc, doc, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getAuth, signInAnonymously, signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBrvRy59biZZilEgp8HIpHIUA16PcaflJU",
-    authDomain: "perangkat-guru-85e8f.firebaseapp.com",
-    projectId: "perangkat-guru-85e8f",
-    storageBucket: "perangkat-guru-85e8f.firebasestorage.app",
-    messagingSenderId: "489186826165",
-    appId: "1:489186826165:web:c7b6d6a91ec70d12f06bfc",
-    measurementId: "G-6K1DGGKEES"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBrvRy59biZZilEgp8HIpHIUA16PcaflJU",
+  authDomain: "perangkat-guru-85e8f.firebaseapp.com",
+  projectId: "perangkat-guru-85e8f",
+  storageBucket: "perangkat-guru-85e8f.firebasestorage.app",
+  messagingSenderId: "489186826165",
+  appId: "1:489186826165:web:c7b6d6a91ec70d12f06bfc",
+  measurementId: "G-6K1DGGKEES"
+};
 
-  const app = initializeApp(firebaseConfig);
-  window.db = getFirestore(app);
-  window.auth = getAuth(app);
-  
-  window.fs = { 
-    collection, getDocs, setDoc, doc, deleteDoc, updateDoc,
-    signInAnonymously, signOut 
-  };
-  
-  window.dispatchEvent(new Event('firebase-ready'));
-})();
+const app = initializeApp(firebaseConfig);
+window.db = getFirestore(app);
+window.auth = getAuth(app);
+
+window.fs = { 
+  collection, getDocs, setDoc, doc, deleteDoc, updateDoc,
+  signInAnonymously, signOut 
+};
+
+window.dispatchEvent(new Event('firebase-ready'));
